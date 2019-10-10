62690dc48a3996a98ce09f47abd2ee3f80b7f0fa
playrey2010 <playrey2010@gmail.com>
2019-10-10 15:29:07 -0400

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    // form for category, form for car, update edit delete for both(?)
+}
